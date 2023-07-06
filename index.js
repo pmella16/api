d136863246922f06b4617cc03cc7d8123a20f67b
pablo mella <fire_crow@hotmail.com>
2023-07-05 23:55:46 -0400

    add upload avatar

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
 
 const app = express();
 const PORT = 5555;
+
+// enable files upload
+app.use(fileUpload({
+  createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 
 app.get('/xml', async (req, res, next) => {
@@ -12,6 +28,36 @@ app.get('/upload', async (req, res, next) => {
   res.send('upload');
 });
 
+
+app.post('/upload-avatar', async (req, res) => {
+  try {
+      if(!req.files) {
+          res.send({
+              status: false,
+              message: 'No file uploaded'
+          });
+      } else {
+          //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+          let avatar = req.files.avatar;
+          
+          //Use the mv() method to place the file in the upload directory (i.e. "uploads")
+          avatar.mv('./uploads/' + avatar.name);
+
+          //send response
+          res.send({
+              status: true,
+              message: 'File is uploaded',
+              data: {
+                  name: avatar.name,
+                  mimetype: avatar.mimetype,
+                  size: avatar.size
+              }
+          });
+      }
+  } catch (err) {
+      res.status(500).send(err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
